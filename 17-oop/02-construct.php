099bf083be26f18781d190d260b49a32db3c94dc
sebastian <sebastianramirezagudelo442@gmail.com>
2025-11-28 10:32:44 -0500

    oop

--- 17-oop/02-construct.php
+++ 17-oop/02-construct.php
@@ -1,53 +1,49 @@
 <?php
-
-$title  = '02-constructor ';
+$title = '02-construct';
 $descripcion = 'Special method that initializes a new object upon creation.';
 
-
 include 'template/header.php';
-echo "<section>";
 
-class PlayList {
-    # Attributes
+echo '<section>';
+
+class PlayList{
+    # Attrs
     public $name;
     public $artist = array();
     public $genre = array();
     public $image = array();
-    public $year =  array();
-    # Construct Methods
+    public $year = array();
 
-    public function __construct($name) {
+    # Construct Method
+    public function __construct($name){
         $this->name = $name;
     }
-    public function setPlayList($artist,$genre,$image,$year) {
+    public function setPlayList($artist, $genre, $image, $year){
         $this->artist[] = $artist;
         $this->genre[] = $genre;
         $this->image[] = $image;
         $this->year[] = $year;
     }
-
-    public function getPlayList() {
+    public function getPlayList(){
         echo "<h3>PlayList: $this->name</h3>
-                <div style='display: flex; gap: 0.4rem; flex-direction: column'>";
-                foreach($this->artist as $i => $artist) {
-                echo "<div style='display: flex; gap: 0.4rem; border: 2px solid #0009; background-color: #0003;'>";
-                    <img src='{$this->image[$i]}' width='100px'>
+            <div style='display:flex; gap: 0.4rem; flex-direction: column'>";
+        foreach ($this->artist as $i => $artist) {
+            echo "<div style='display: flex; gap: 0.4rem; border: 2px solid #0009; background:#0003'>
+                    <img src='{$this->image[$i]}' width='160px'>
                     <div>
-                        <h4>Artist: {$artist}</h4>
+                        <h4>Artist: $artist</h4>
                         <h5>Genre: {$this->genre[$i]}</h5>
-                        <h5></h5>
-                    
-                    
+                        <h5>Year: {$this->year[$i]}</h5>
                     </div>
-            </div>";
-                
-                
-            }
-
+                    </div>";
+        }
+        echo "</div>";
     }
 }
-
-$pl = new PlayList('Juanes','La camisa negra',1998,'La tierra');
-echo $pl->song;
+$pl = new PlayList('Julio Jaramillo');
+$pl->setPlayList('Fatalidad', 'Bolero', 'https://acortar.link/jk1qao', 1956);
+$pl->setPlayList('Nuestro Juramento', 'Bolero', 'https://acortar.link/RUvknH', 1957);
+$pl->setPlayList('Te Esperaré', 'Bolero', 'https://acortar.link/bSWgNq', 1948);
+$pl->getPlayList();
 
 include 'template/footer.php';
--- 17-oop/03-private.php
+++ 17-oop/03-private.php
@@ -7,4 +7,43 @@ $descripcion = 'Restricts property or method access to only within its class.';
 include 'template/header.php';
 echo "<section>";
 
+class RenderTable
+{
+    private $nr; #Number of Rows
+    private $nc; #Number of Columns
+
+    public function __construct($nr, $nc)
+    {
+        $this->nr = $nr;
+        $this->nc = $nc;
+        //Access to private methods 
+        $this->startTable();
+        $this->bodyTable();
+        $this->closeTable();
+    }
+    private function startTable()
+    {
+        echo "<table>";
+    }
+    private function bodyTable()
+    {
+        echo "<h3> Table ({$this->nr}x{$this->nc})</h3>";
+        for ($r = 1; $r <= $this->nr; $r++) {
+            echo "<tr>";
+            for ($c = 1; $c <= $this->nc; $c++) {
+                echo "<td> f{$r}c{$c} </td>";
+            }
+            echo "</tr>";
+        }
+    }
+    private function closeTable()
+    {
+        echo "</table>";
+    }
+}
+$tbl = new RenderTable(5, 5);
+$tbl = new RenderTable(3, 8);
+$tbl = new RenderTable(2, 2);
+
+
 include 'template/footer.php';
